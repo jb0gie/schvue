7fa2633126138f0f8a1da0b833b67d06c8a26a80
j_b0gie <wxzvrd@protonmail.com>
2024-01-18 15:22:26 +0000

    what a schvue

---
+++ component/common.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="p-4">
+    <div
+      class="navbar rounded-full bg-gradient-to-r from-[#247420] to-[#420247]"
+    >
+      <div class="navbar-start">
+        <div class="text-4xl font-bold">
+          <code
+            style="
+              color: transparent;
+              background-clip: text;
+              -webkit-background-clip: text;
+              background-image: url('https://media3.giphy.com/media/zkrFC9UU5O9wxQJ69s/giphy.gif');
+            "
+          >
+            schweb3
+          </code>
+          <span class="text-pink-300">.</span>
+          <code>
+            <span
+              style="
+                color: transparent;
+                background-clip: text;
+                -webkit-background-clip: text;
+                background-image: url('https://media2.giphy.com/media/bTDdkhpX34Hwp1eaiS/200w.webp?cid=ecf05e47havsfdo7vnub33cofwhw8suyhqvh1e5t1wr1veb4&rid=200w.webp&ct=g');
+              "
+            >
+              247
+            </span>
+            <span
+              style="
+                color: transparent;
+                background-clip: text;
+                -webkit-background-clip: text;
+                background-image: url('https://i.giphy.com/media/5xtDarMq3ckPLlJtUHK/giphy-downsized-large.gif');
+              "
+            >
+              420
+            </span>
+          </code>
+          <span class="text-pink-300">.</span>
+          <code
+            style="
+              color: transparent;
+              background-clip: text;
+              -webkit-background-clip: text;
+              background-image: url('https://media2.giphy.com/media/JlVxiw2MrW4DwKeG4e/giphy.gif?cid=ecf05e47havsfdo7vnub33cofwhw8suyhqvh1e5t1wr1veb4&rid=giphy.gif&ct=g');
+            "
+          >
+            xyz
+          </code>
+        </div>
+        <div>
+          <img
+            src="https://image.flaticon.com/icons/svg/497/497348.svg"
+            alt
+            class="w-8"
+          />
+        </div>
+      </div>
+      <div class="navbar-end">
+        <div class="avatar avatar-ring avatar-md">
+          <div class="dropdown-container">
+            <div class="dropdown">
+              <label
+                class="btn btn-ghost flex cursor-pointer px-0"
+                tabindex="0"
+              >
+                <img
+                  src="https://mastodon.247420.xyz/system/accounts/avatars/109/473/867/332/038/427/original/4b3de76134039d66.png"
+                  alt="avatar"
+                />
+              </label>
+              <div class="dropdown-menu dropdown-menu-bottom-left">
+                <router-link
+                  tabindex="-1"
+                  class="dropdown-item text-sm"
+                  to="/welcome"
+                >
+                  welcome
+                </router-link>
+                <router-link
+                  tabindex="-1"
+                  class="dropdown-item text-sm"
+                  to="/home"
+                >
+                  home
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'what a schvue',
+    };
+  },
+};
+</script>
---
+++ component/welcome.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="flex flex-wrap">
+    <div class="w-full sm:w-8/12 mb-10">
+      <div class="container mx-auto h-full sm:p-10">
+        <nav class="flex px-4 justify-between items-center">
+          <h1 class="text-6xl lg:text-7xl font-bold">
+            It's only a
+            <span class="text-pink-300">pink</span>
+            🐸❗
+          </h1>
+        </nav>
+        <div class="w-20 h-2 bg-pink-500 my-4"></div>
+        <header class="container px-4 lg:flex mt-10 items-center lg:mt-0">
+          <div class="w-full">
+            <p class="text-xl mb-10">
+              Schwepe was not always the lord of the metaverse. He was once a
+              humble programmer who worked for a company called Meta. One day,
+              he accidentally spilled a can of Schwepe's on his keyboard, which
+              caused a glitch in the code he was writing.
+            </p>
+            <p class="text-xl m-5">
+              The glitch gave him access to the core of the metaverse, where he
+              discovered a hidden power that allowed him to manipulate reality.
+              He decided to use his power to create a new world in his own
+              image, where everything was pink and sparkly.
+            </p>
+            <p class="text-xl m-5">
+              He also changed his name to Schwepe, after his favorite drink, and
+              declared himself the lord and ironic creator of the metaverse. He
+              now rules over his pink domain with an iron fist, and anyone who
+              dares to oppose him will face his wrath. Schwepe is feared and
+              revered by all, but secretly, he is lonely and bored. He wishes he
+              had someone to share his pink world with, someone who would
+              appreciate his sense of humor and his love for Schweppes.
+            </p>
+            <button
+              class="bg-green-500 text-white text-2xl font-medium px-4 py-2 rounded shadow"
+            >
+              Learn More
+            </button>
+          </div>
+        </header>
+      </div>
+    </div>
+    <img
+      src="https://cdn.discordapp.com/attachments/1094571932088807547/1161277816487288932/the_real_cheese_man_pink_Pepe_frog_wearing_sunglasses_cd6ca716-253b-4d8f-803a-a69c51418190.png?ex=65b8ea3e&is=65a6753e&hm=f70bdd941e9c8c1f677c8ed40d3c73a897a7642c94978288a1c7e100a1dced9b&"
+      alt="Leafs"
+      class="w-full h-48 object-cover sm:h-screen sm:w-4/12"
+    />
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
